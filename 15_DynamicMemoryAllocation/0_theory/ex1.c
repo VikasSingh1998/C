add8c10723856d51cb834f933056a7e4e26db202
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 11:53:09 +0530

    Update ex1.c

--- 15_DynamicMemoryAllocation/0_theory/ex1.c
+++ 15_DynamicMemoryAllocation/0_theory/ex1.c
@@ -1,3 +1,31 @@
 Dynamic Memory Allocation ==> Dynamic memory allocation is used to allocate the memory at run time.
 Static memory allocation ==> static memory allocation is used to allocate the memory at compile time.
 --------------------------------------------------------------
+In C, dynamic memory allocation is achieved through functions like: malloc(),calloc(),realloc(),free()
+=======================================================================================================
+1. malloc (Memory Allocation):
+----------------------------------
+Syntax:
+    void* malloc(size_t size);
+
+==> Allocates a block of memory of the specified size in bytes.
+==> Returns a void pointer to the beginning of the block or NULL if the allocation fails.
+==> Does not initialize memory; contains garbage values.
+
+Example: 
+int *ptr = (int *)malloc(5 * sizeof(int)); // Allocates memory for 5 integers
+
+SO, Here malloc return the void*, so we will type cast to int*.
+========================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+  
