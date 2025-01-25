e8d412f6810ce817506503cf13f7ac9b7ba41c44
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 13:38:46 +0530

    Update ex2.c

--- 15_DynamicMemoryAllocation/2_calloc/ex2.c
+++ 15_DynamicMemoryAllocation/2_calloc/ex2.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int *arr;
+    int n = 5;
+
+    // Allocate memory using calloc
+    arr = (int*)calloc(n, sizeof(int));
+
+    //calloc will return NULL if it fails to allocate the Memory.
+    if (arr == NULL) 
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    // All values initialized to 0
+    printf("Using calloc: ");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]); // Contains 0
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
 
