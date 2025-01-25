67a17f5966e5cd9f9a54cb075f7bf173e6394e85
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 13:16:59 +0530

    Update ex1.c

--- 15_DynamicMemoryAllocation/0_theory/ex1.c
+++ 15_DynamicMemoryAllocation/0_theory/ex1.c
@@ -3,7 +3,7 @@ Static memory allocation ==> static memory allocation is used to allocate the me
 --------------------------------------------------------------
 In C, dynamic memory allocation is achieved through functions like: malloc(),calloc(),realloc(),free()
 =======================================================================================================
-1. malloc (Memory Allocation):
+1. malloc() ==> Memory Allocation
 ----------------------------------
 Syntax:
     void* malloc(size_t size);
@@ -17,8 +17,69 @@ int *ptr = (int *)malloc(5 * sizeof(int)); // Allocates memory for 5 integers
 
 SO, Here malloc return the void*, so we will type cast to int*.
 ========================================================================================================
+2. calloc() ==> Contiguous Allocation
+---------------------------------------
+Syntax:
+void* calloc(size_t num, size_t size);
+num ==> number of element 
+size ==> size of element.
+
+==> Allocates memory for an array of elements and initializes them to zero.
+==> Returns a pointer to the beginning of the allocated memory.
+==> It initialize the memory with 0.
+
+Example:
+    int *ptr = (int *)calloc(5, sizeof(int)); // Allocates memory for 5 integers and initializes them to 0
+
+Since calloc() return the void*, so we have to typecast to int*.
+========================================================================================================
+Q: While both malloc() and calloc() are used for dynamic memory allocation in C, there are some key differences between them?
+Ans ==> 
+
+    Feature	                     malloc()	                                                                calloc()
+Purpose	               Allocates a single block of memory.	                                    Allocates memory for an array of elements.
+Initialization	       Does not initialize the allocated memory (contains garbage values).	    Initializes all allocated memory to 0.
+Syntax	               void* malloc(size_t size)	                                            void* calloc(size_t num, size_t size)
+Arguments	           Takes a single argument: total size in bytes.	                        Takes two arguments: number of elements and size of each element.
+Performance	           Slightly faster as it doesn’t initialize memory.	                        Slightly slower due to initialization overhead.
+
+===========================================================================================================================
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
