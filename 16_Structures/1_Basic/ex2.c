1d1deb6ea4fe4f8aaf48e67ede1d62884709fefc
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 15:50:35 +0530

    Update ex2.c

--- 16_Structures/1_Basic/ex2.c
+++ 16_Structures/1_Basic/ex2.c
@@ -1,1 +1,19 @@
+// Example 1: Basic structure
+#include <stdio.h>
+
+struct Person 
+{
+    char name[50];
+    int age;
+};
+
+int main() 
+{
+    struct Person p1 = {"Alice", 30};
+    printf("Name: %s, Age: %d\n", p1.name, p1.age);
+    return 0;
+}
+/*
+Name: Alice, Age: 30
+*/
 
