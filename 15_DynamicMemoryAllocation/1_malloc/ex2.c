b7ba958d42619b3bae82ded3b845eb507448c3fb
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 13:19:17 +0530

    Update ex2.c

--- 15_DynamicMemoryAllocation/1_malloc/ex2.c
+++ 15_DynamicMemoryAllocation/1_malloc/ex2.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int *arr;
+    int n = 5;
+
+    // Allocate memory using malloc
+    arr = (int *)malloc(n * sizeof(int));
+    
+    //malloc will return NULL, if Memory allocation fail.
+    if (arr == NULL) 
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    // Garbage values (memory not initialized)
+    printf("Using malloc: ");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]); // Likely contains garbage values
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
+
 
