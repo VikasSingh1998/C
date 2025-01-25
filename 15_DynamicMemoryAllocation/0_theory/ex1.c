fcc866f220ddb3d08620f6f2aa8286ea309bb683
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 11:39:00 +0530

    Update ex1.c

--- 15_DynamicMemoryAllocation/0_theory/ex1.c
+++ 15_DynamicMemoryAllocation/0_theory/ex1.c
@@ -1,1 +1,3 @@
-
+Dynamic Memory Allocation ==> Dynamic memory allocation is used to allocate the memory at run time.
+Static memory allocation ==> static memory allocation is used to allocate the memory at compile time.
+--------------------------------------------------------------
