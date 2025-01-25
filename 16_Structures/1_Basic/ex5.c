401fa484fa70bd6a5f61f3907ba1b0fb3fb50e60
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 16:07:21 +0530

    Update ex5.c

--- 16_Structures/1_Basic/ex5.c
+++ 16_Structures/1_Basic/ex5.c
@@ -1,1 +1,24 @@
+// Example 3: Nested structure
+#include <stdio.h>
 
+struct Address 
+{
+    char district[50];
+    char village[50];
+};
+
+struct Person 
+{
+    char name[50];
+    struct Address address;
+};
+
+int main() 
+{
+    struct Person p1 = {"Alice", {"Nashik", "Ghoti"}};
+    printf("Name: %s, District: %s, Village: %s\n", p1.name, p1.address.district, p1.address.village);
+    return 0;
+}
+/*
+Name: Alice, District: Nashik, Village: Ghoti
+*/
