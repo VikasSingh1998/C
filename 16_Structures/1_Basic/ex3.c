b3094245c87bcd740125eca0e14d7d73c47da6fc
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 15:51:19 +0530

    Update ex3.c

--- 16_Structures/1_Basic/ex3.c
+++ 16_Structures/1_Basic/ex3.c
@@ -1,1 +1,29 @@
+// Example 1: Basic structure
+#include <stdio.h>
+#include <string.h>
+
+struct Person 
+{
+    char name[50];
+    int age;
+};
+
+int main() 
+{
+    struct Person p1;
+    /*
+    p1.name = "vikas"; ==> Error
+    In C, you cannot directly assign a string literal to a character array using the = operator. 
+    Instead, you need to use the strcpy() function from <string.h> to copy the string into the array.
+    */
+    // Use strcpy to assign a string to the name array
+    strcpy(p1.name, "vikas");
+    p1.age = 20;
+    
+    printf("Name: %s, Age: %d\n", p1.name, p1.age);
+    return 0;
+}
+/*
+Name: Vikas, Age: 20
+*/
 
