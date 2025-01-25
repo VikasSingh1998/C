9c2015f43c472a58e918909cf1e6ae9a2f4fe41c
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 16:09:04 +0530

    Update ex6.c

--- 16_Structures/1_Basic/ex6.c
+++ 16_Structures/1_Basic/ex6.c
@@ -1,1 +1,17 @@
+// Example 4: Pointer to structure
+#include <stdio.h>
+
+struct Person 
+{
+    char name[50];
+    int age;
+};
+
+int main() 
+{
+    struct Person p1 = {"Alice", 30};
+    struct Person* ptr = &p1;
+    printf("Name: %s, Age: %d\n", ptr->name, ptr->age);
+    return 0;
+}
 
