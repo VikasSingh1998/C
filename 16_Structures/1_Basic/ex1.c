a9e5d52bd43f6d79768f38cd793b12f976465dc0
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 15:36:15 +0530

    Update ex1.c

--- 16_Structures/1_Basic/ex1.c
+++ 16_Structures/1_Basic/ex1.c
@@ -1,1 +1,9 @@
+1. Structures in C
+--------------------
+A struct is a user-defined data type in C that allows grouping different types of data. 
+Structures are helpful in managing related data under one name, and each member can have a different data type.
+=====================================================================================================================
+
+
+
 
