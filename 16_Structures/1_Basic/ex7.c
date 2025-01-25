db54e0a2d8a3dd0d0e672b088adc56dd86a14893
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-25 16:14:08 +0530

    Update ex7.c

--- 16_Structures/1_Basic/ex7.c
+++ 16_Structures/1_Basic/ex7.c
@@ -1,1 +1,26 @@
+// Example 5: Returning structure from a function
+#include <stdio.h>
 
+struct Person 
+{
+    char name[50];
+    int age;
+};
+
+struct Person createPerson(char* name, int age) 
+{
+    struct Person p;
+    snprintf(p.name, sizeof(p.name), "%s", name);
+    p.age = age;
+    return p;
+}
+
+int main() 
+{
+    struct Person p1 = createPerson("Alice", 30);
+    printf("Name: %s, Age: %d\n", p1.name, p1.age);
+    return 0;
+}
+/*
+Name: Alice, Age: 30
+*/
