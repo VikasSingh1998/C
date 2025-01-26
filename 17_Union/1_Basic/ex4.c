9064eab08be76201e5cac498779076167c571998
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-26 13:33:45 +0530

    Update ex4.c

--- 17_Union/1_Basic/ex4.c
+++ 17_Union/1_Basic/ex4.c
@@ -1,1 +1,32 @@
+// Example 3: Union with a Struct
+#include <stdio.h>
 
+struct Person 
+{
+    char name[50];
+    int age;
+};
+
+union Data 
+{
+    int i;
+    struct Person person;
+};
+
+int main() 
+{
+    union Data data;
+
+    data.i = 10; // Assign an integer
+    printf("Data.i: %d\n", data.i);
+
+    snprintf(data.person.name, sizeof(data.person.name), "Alice"); // Assign struct values
+    data.person.age = 30;
+    printf("Name: %s, Age: %d\n", data.person.name, data.person.age);
+    
+    return 0;
+}
+/*
+Explanation:
+The memory for data.i and data.person is shared. Assigning values to data.person overwrites data.i.
+*/
