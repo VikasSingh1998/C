227086b6e09e8447e83a060742b1691a8bd50b68
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-26 16:10:32 +0530

    Update ex2.c

--- 18_Enumerations/1_Basic/ex2.c
+++ 18_Enumerations/1_Basic/ex2.c
@@ -1,1 +1,33 @@
+//Enum with typedef.
+#include <stdio.h>
+
+typedef enum 
+{
+    CONNECTED,
+    DISCONNECTED,
+    ERROR
+} ConnectionState;
+
+void checkConnection(ConnectionState state) 
+{
+    switch (state) 
+    {
+        case CONNECTED:
+            printf("Device is connected\n");
+            break;
+        case DISCONNECTED:
+            printf("Device is disconnected\n");
+            break;
+        case ERROR:
+            printf("Connection error\n");
+            break;
+    }
+}
+
+int main() 
+{
+    ConnectionState state = CONNECTED;
+    checkConnection(state);
+    return 0;
+}
 
