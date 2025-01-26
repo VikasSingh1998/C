ad3123dc1ea3e26e13134bf6a62e110542613b12
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-26 12:40:50 +0530

    Update ex2.c

--- 17_Union/1_Basic/ex2.c
+++ 17_Union/1_Basic/ex2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+union Data 
+{
+    int i;
+    float f;
+    char str[20];
+};
+
+int main() {
+    union Data data;
+    
+    data.i = 10; // Assign an integer
+    printf("Data.i: %d\n", data.i); // Output: Data.i: 10
+    
+    data.f = 220.5; // Assign a float, overwrites 'i'
+    printf("Data.f: %.2f\n", data.f); // Output: Data.f: 220.50
+    
+    // data.str = "vikas";//Error
+    strcpy(data.str,"vikas");
+    printf("String value is: %s",data.str);
+    
+    return 0;
+}
 
