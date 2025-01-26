d3aff4b66eb55a0681a077629f9960391e99787a
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-26 16:03:39 +0530

    Update ex1.c

--- 18_Enumerations/1_Basic/ex1.c
+++ 18_Enumerations/1_Basic/ex1.c
@@ -1,1 +1,17 @@
+Enum ==> Enum is a user-defined data type consisting of integral constants.
+--------------------------
+Example:
+enum Weekday 
+{
+    Sunday, //0
+    Monday, //1
+    Tuesday, 
+    Wednesday = 5, 
+    Thursday, 
+    Friday, 
+    Saturday
+};
+// Enum values (Sunday, Monday, etc.) are automatically assigned integer values starting from 0.
+// By explicitly setting Wednesday = 5, subsequent values increment from 1 (Thursday = 6, Friday = 7, and so on).
+  
 
