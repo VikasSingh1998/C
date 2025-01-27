badc906bbba62d7fcbbc5960c8040c988a96b764
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-01-27 23:41:38 +0530

    Update ex1.c

--- 19_Typedef_AND_TypeAliases/1_Basic/ex1.c
+++ 19_Typedef_AND_TypeAliases/1_Basic/ex1.c
@@ -1,1 +1,34 @@
+typedef ==> typedef is used in C and C++ to create an alias name for existing data types
+---------------------------------------------------------------------------------------------
+Example 1:
+ typedef unsigned int uint; ==> we can use uint instead of the unsigned int.
+--------------------------------------
+Example 2:
+typedef struct 
+{
+    char name[50];
+    int age;
+} Person;
+
+Explanation:
+==> typedef eliminates the need to write struct every time you declare a variable of type Person.
+Person p = {"Alice", 30};  // Use 'Person' instead of 'struct Person'
+  
+==> Without typedef
+struct Person 
+{
+    char name[50];
+    int age;
+};
+and declare variables like ==> struct Person p;
+-----------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 
